712d0c1c9e329166a5dd828ceb9e4da4d38cbdb9
cesar <cperes@sprintmind.com.br>
2020-04-04 11:05:16 -0300

    Criação das entidades

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,27 @@
+﻿using QuickBuy.Dominio.Entidades.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string Cep { get; set; }
+        public string Estado { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public string NumeroEndereco { get; set; }
+        /// <summary>
+        /// Pedido deve ter pelo menos um ou muitos itens de pedido
+        /// </summary>
+
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+
+          
+    }
+}
