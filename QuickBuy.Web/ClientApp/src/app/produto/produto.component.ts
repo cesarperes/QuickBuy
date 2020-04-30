c65e24b25e0a4bea6268252d73b505d268ad96b7
cesar <cperes@sprintmind.com.br>
2020-04-30 00:37:39 -0300

    Mod 9 completo

--- QuickBuy.Web/ClientApp/src/app/produto/produto.component.ts
+++ QuickBuy.Web/ClientApp/src/app/produto/produto.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
+import { Component, OnInit } from '@angular/core';
+import { Produto } from "../modelo/produto";
+import { ProdutoServico } from '../servicos/produto/produto.servico';
 
 @Component({
   selector: "app-produto",
-  template: "<html><body>{{ obterNome() }}</body></html>"
+  templateUrl: "./produto.component.html",
+  styleUrls: ["./produto.component.css"]
 })
-export class ProdutoComponent {
-  public nome: string;
-  public liberadoParaVenda: boolean;
+export class ProdutoComponent implements OnInit {
+  public produto: Produto;
 
-  public obterNome(): string {
-    return "Samsung";
+  constructor(private produtoServico: ProdutoServico) {
+
+  }
+
+  ngOnInit(): void {
+    this.produto = new Produto();
+  }
+
+  public cadastrar() {
+    this.produtoServico.cadastrar(this.produto)
+      .subscribe(
+        produtoJson => {
+          console.log(produtoJson);
+        },
+        e => {
+          console.log(e.error);
+        }
+      );
   }
 }
